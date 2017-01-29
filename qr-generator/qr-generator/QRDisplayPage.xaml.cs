4099b0d9d7ee78e085be5f07f2495cbf14868409
Cindy Xiao <cndxiao@gmail.com>
2017-01-28 20:59:39 -0700

    Add display of consecutive QR frames.

--- qr-generator/qr-generator/QRDisplayPage.xaml.cs
+++ qr-generator/qr-generator/QRDisplayPage.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -20,32 +22,54 @@ namespace qr_generator
     {
 
         public string dataSizeText { get; set; }
+        public string dataContentsText { get; set; }
+
+        private System.Threading.Timer _timer;
+
+        private string[] inputDataArray = {"ABCDEFGH22", "ABDFERGS43", "0123456789", "DEADBEEF10"};
+        private int inputDataIndex = 0;
+
+
+        private BarcodeWriter barcodeWriter = new BarcodeWriter
+        {
+            Format = ZXing.BarcodeFormat.QR_CODE,
+            Options = new ZXing.Common.EncodingOptions
+            {
+                Width = 300,
+                Height = 300,
+                Margin = 30
+            }
+        };
 
         public QRDisplayPage()
         {
             this.InitializeComponent();
+            _timer = new System.Threading.Timer(new System.Threading.TimerCallback((obj) => Refresh()), null, 0, 2000);
         }
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
+        }
 
-            var barcodeWriter = new BarcodeWriter
+        private async void Refresh()
+        {
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
             {
-                Format = ZXing.BarcodeFormat.QR_CODE,
-                Options = new ZXing.Common.EncodingOptions
+                if(inputDataIndex < 4)
                 {
-                    Width = 300,
-                    Height = 300,
-                    Margin = 30
+                    var inputData = inputDataArray[inputDataIndex];
+                    var image = barcodeWriter.Write(string.Concat(Enumerable.Repeat(inputData, 50)));
+                    imageBarcode.Source = image;
+                    dataSizeText = (inputData.Length * sizeof(Char)).ToString();
+                    dataContentsText = inputData;
+                    inputDataIndex++;
+                } else
+                {
+                    inputDataIndex = 0;
                 }
-            };
 
-            string inputData = String.Concat(Enumerable.Repeat("DEADBEEF10", 40));
-            var image = barcodeWriter.Write(inputData);
+            });
 
-            imageBarcode.Source = image;
-
-            dataSizeText = (inputData.Length * sizeof(Char)).ToString();
         }
     }
 }
