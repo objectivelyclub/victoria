b61f16936728148784a8954658db321a9ca6db49
Cindy Xiao <cndxiao@gmail.com>
2017-01-28 21:40:51 -0700

    Add dedicated list for display of QR images.

--- qr-generator/qr-generator/QRDisplayPage.xaml.cs
+++ qr-generator/qr-generator/QRDisplayPage.xaml.cs
@@ -29,14 +29,17 @@ namespace qr_generator
         private string[] inputDataArray = {"ABCDEFGH22", "ABDFERGS43", "0123456789", "DEADBEEF10"};
         private int inputDataIndex = 0;
 
+        private List<Windows.UI.Xaml.Media.Imaging.WriteableBitmap> imageQueue = new List<Windows.UI.Xaml.Media.Imaging.WriteableBitmap>();
+        private List<int> dataSizeInfoQueue = new List<int>();
+        private List<string> fakeInputDataList = new List<string>();
 
         private BarcodeWriter barcodeWriter = new BarcodeWriter
         {
             Format = ZXing.BarcodeFormat.QR_CODE,
             Options = new ZXing.Common.EncodingOptions
             {
-                Width = 300,
-                Height = 300,
+                Width = 750,
+                Height = 750,
                 Margin = 30
             }
         };
@@ -44,30 +47,42 @@ namespace qr_generator
         public QRDisplayPage()
         {
             this.InitializeComponent();
-            _timer = new System.Threading.Timer(new System.Threading.TimerCallback((obj) => Refresh()), null, 0, 2000);
         }
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
+            GenerateBitmap();
+            _timer = new System.Threading.Timer(new System.Threading.TimerCallback((obj) => Refresh()), null, 0, 2000);
+        }
+
+        private void GenerateBitmap()
+        {
+            foreach (var inputData in inputDataArray)
+            {
+                string fakeInputData = string.Concat(Enumerable.Repeat(inputData, 50));
+                var fakeInputDataSize = fakeInputData.Length * sizeof(char);
+                var image = barcodeWriter.Write(fakeInputData);
+
+                dataSizeInfoQueue.Add(fakeInputDataSize);
+                imageQueue.Add(image);
+            }
         }
 
         private async void Refresh()
         {
             await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
             {
-                if(inputDataIndex < 4)
-                {
-                    var inputData = inputDataArray[inputDataIndex];
-                    var image = barcodeWriter.Write(string.Concat(Enumerable.Repeat(inputData, 50)));
-                    imageBarcode.Source = image;
-                    dataSizeText = (inputData.Length * sizeof(Char)).ToString();
-                    dataContentsText = inputData;
-                    inputDataIndex++;
-                } else
+                if (inputDataIndex >= imageQueue.Count)
                 {
                     inputDataIndex = 0;
                 }
-
+                dataSizeText = dataSizeInfoQueue[inputDataIndex].ToString();
+                var image = imageQueue[inputDataIndex];
+                if(image != null)
+                {
+                    imageBarcode.Source = image;
+                }
+                inputDataIndex++;
             });
 
         }
