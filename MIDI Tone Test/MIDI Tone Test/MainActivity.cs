e6aac0c9fa11ea11559ca34ecf78d753e2804bc2
Rijesh Augustine <rijesh@ualberta.ca>
2017-01-28 17:37:06 -0700

    Added a jetplayer that sort of weeks.

---
+++ MIDI Tone Test/MIDI Tone Test/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Media;
+using System.IO;
+
+namespace MIDI_Tone_Test
+{
+    [Activity(Label = "MIDI_Tone_Test", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+            JetPlayer jetPlayer = JetPlayer.GetJetPlayer();
+            //jetPlayer.LoadJetFile("/storage/AE2D-8B85/jone.jet");
+            JetPlayer jetPlayer2 = JetPlayer.GetJetPlayer();
+            jetPlayer2.LoadJetFile("/storage/AE2D-8B85/jone2.jet");
+            sbyte segmentId = 0;
+
+           // bool exists = File.Exists("/storage/AE2D-8B85/jone.jet");
+
+            while (true)
+            {
+                jetPlayer2 = JetPlayer.GetJetPlayer();
+                jetPlayer2.LoadJetFile("/storage/AE2D-8B85/jone2.jet");
+                jetPlayer2.QueueJetSegment(1, -1, 10, -1, 0, segmentId++);
+                jetPlayer2.Play();
+                System.Threading.Thread.Sleep(2000);
+                jetPlayer2.Pause();
+                jetPlayer2.Release();
+
+                jetPlayer2 = JetPlayer.GetJetPlayer();
+                jetPlayer2.LoadJetFile("/storage/AE2D-8B85/jone2.jet");
+                jetPlayer2.QueueJetSegment(1, -1, 10, -1, 0, segmentId++);
+                jetPlayer2.Play();
+                System.Threading.Thread.Sleep(2000);
+                jetPlayer2.Pause();
+                jetPlayer2.Release();
+            }
+
+            bool here = false;
+
+
+            //jetPlayer2.LoadJetFile("/storage/AE2D-8B85/jone.jet");
+            //jetPlayer2.QueueJetSegment(4, -1, 10, -1, 0, segmentId++);
+            //jetPlayer.Pause();
+            //jetPlayer2.Play();
+
+
+        }
+    }
+}
+
--- master/master/MainActivity.cs
+++ master/master/MainActivity.cs
@@ -12,7 +12,7 @@ namespace master
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
         }
     }
 }
